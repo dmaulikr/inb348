dc1d6203dbdbc4de5e7ba073e601c02eb5952364
Dongmin Lee <reoim10@gmail.com>
2014-08-27 12:06:22 +1000

    Initial Commit

---
+++ inb348Tests/inb348Tests.m
@@ -0,0 +1,34 @@
+//
+//  inb348Tests.m
+//  inb348Tests
+//
+//  Created by DONGMIN LEE on 27/08/2014.
+//  Copyright (c) 2014 Dongmin Lee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface inb348Tests : XCTestCase
+
+@end
+
+@implementation inb348Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
